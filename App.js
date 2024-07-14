da5e2c0dbe26799d824a5a8e2a7b2bd4e49f09b6
Rehan7723 <rehanshariff170@gmail.com>
2024-07-14 19:28:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Middle from "./components/Middle";
+import Card from "./components/Card";
+//import img1 from "./images/Group 77.png";
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Middle/>
+      <Card/>
+      
+    </div>
+  );
+}
+
+export default App;
